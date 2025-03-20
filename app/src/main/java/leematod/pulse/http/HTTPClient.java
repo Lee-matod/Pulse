05273ad5172fc214a447e5219f5673600dc93c30
Lee-matod <lee.underscore.matod@gmail.com>
2025-03-20 17:27:36 +0100

    Add straplines to mask

--- app/src/main/java/leematod/pulse/http/HTTPClient.java
+++ app/src/main/java/leematod/pulse/http/HTTPClient.java
@@ -118,8 +118,9 @@ public class HTTPClient {
                 "contents.twoColumnBrowseResultsRenderer(secondaryContents.sectionListRenderer."
                         + "contents.musicShelfRenderer.contents."
                         + Innertube.MASK_MUSIC_RESPONSIVE_LIST_ITEM_RENDERER
-                        + ",tabs.tabRenderer.content.sectionListRenderer.contents."
+                        + ",tabs.tabRenderer.content.sectionListRenderer.contents("
                         + Innertube.MASK_MUSIC_RESPONSIVE_HEADER_RENDERER
+                        + ",musicResponsiveHeaderRenderer(straplineTextOne,straplineThumbnail))"
                         + "),trackingParams");
 
         BrowseResponse response;
